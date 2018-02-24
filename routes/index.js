3c61102df5350a97f069a2706f7acc389b34d569
Joseph Clough <joeclough@josephs-mbp.home>
2018-02-24 18:07:35 +0000

    Backend Authentication Complete

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 //Home Page
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
     res.render('index');
 });
+
+//Welcome Page
+router.get('/welcome', (req, res) => {
+  res.render('welcome');
+});
+
+function ensureAuthenticated(req, res, next) {
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		res.redirect('/welcome');
+	}
+}
 
 module.exports = router;
